3744fe661b5c40781919c5387d36f4eac1342146
Ahmed Massoud <ahmed.massoud@aalto.fi>
2018-12-28 19:49:39 +0200

    v1 with email

---
+++ app.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+//const path    = require("path");
+var nodemailer = require('nodemailer');
+const app = express();
+const port = 3000;
+
+//middlewares
+app.set('view engine', 'ejs');
+app.use(express.static('statics'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//routes
+app.get('/', (req, res) => res.render('index'));
+app.get('/confirm', (req, res) => res.render('confirm'));
+
+app.post('/confirm', (req, res) => {
+    // name, decision, count, food, otherfood, gifts, comments, submit
+    var name = req.body.name;
+    var decision = req.body.decision;
+    var count = req.body.count;
+    var food = req.body.food;
+    var otherfood = req.body.otherfood;
+    var gifts = req.body.gifts;
+    var comments = req.body.comments;
+    var htmlmail = `<!DOCTYPE html><html><head></head><body><h1>New Invitation response!</h1> <h4>Dear Marina and Ahmed, you have recieved a new response from ${name} , he says ${decision}</h4> <h5>also find the rest of his answers as the following:</h5> <table style="border: 1px solid black; border-collapse: collapse;"> <tr> <th style="border: 1px solid black; border-collapse: collapse; ">Name</th> <th style="border: 1px solid black; border-collapse: collapse;">Decision</th> <th style="border: 1px solid black; border-collapse: collapse;">Family count</th> <th style="border: 1px solid black; border-collapse: collapse;">Food requested</th> <th style="border: 1px solid black; border-collapse: collapse;">Other food</th> <th style="border: 1px solid black; border-collapse: collapse;">Gifts</th> <th style="border: 1px solid black; border-collapse: collapse;">comments</th> </tr> <tr> <td style="border: 1px solid black; border-collapse: collapse;">${name}</td> <td style="border: 1px solid black; border-collapse: collapse; ">${decision}</td> <td style="border: 1px solid black; border-collapse: collapse;">${count}</td> <td style="border: 1px solid black; border-collapse: collapse;">${food}</td> <td style="border: 1px solid black; border-collapse: collapse;">${otherfood}</td> <td style="border: 1px solid black; border-collapse: collapse;">${gifts}</td> <td style="border: 1px solid black; border-collapse: collapse;">${comments}</td> </tr> </table><h5>I will make sure to keep you updated, if any new responses come!, Have an nice day =)</h5> </body> </html>`
+    var mailOptions = {
+        from: 'ahmedmarinawedding@gmail.com',
+        to: 'ahmed.khairy.mohammed@gmail.com, marremaj@gmail.com',
+       // to:'',
+        subject: 'Sending Email using Node.js',
+        html: htmlmail
+      };
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+
+   // res.render('confirm');
+});
+//emails
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ahmedmarinawedding@gmail.com',
+    pass: 'marina@ahmed123'
+  }
+});
+
+
+
+/* transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); */
+
+
+
+//server
+app.listen(port, () => console.log(`weddingsite app listening on port ${port}!`))
---
+++ statics/wedding.js
@@ -0,0 +1,83 @@
+ var slideIndex = 1;
+showSlides(slideIndex);
+
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  console.log("runned");
+  
+}
+
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+
+  dots[slideIndex-1].className += " active";
+}
+
+/* var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 1000); // Change image every 2 seconds
+} */
+
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("UpBtn").style.display = "block";
+  } else {
+    document.getElementById("UpBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function showIdeas(){
+  document.getElementById("giftLables").style.display = "block";
+  document.getElementById("giftValues").style.display = "block";
+}
+
+function otherpref(){
+    if (document.getElementById("food").selectedIndex == 4  ){
+      console.log(document.getElementById("food").selectedIndex);
+      
+      document.getElementById("other").style.display = "inline";
+
+    }
+}
